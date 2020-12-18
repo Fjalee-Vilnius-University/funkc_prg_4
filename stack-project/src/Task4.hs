68f9758bcf77d26eede57452074c8ed97b2e5b4d
Rytis <cepRytis@gmail.com>
2020-12-18 14:04:12 +0200

    from parse to parseJL

--- stack-project/src/Task4.hs
+++ stack-project/src/Task4.hs
@@ -9,6 +9,13 @@ import Data.Char
 --runState (runExceptT parseInt) "i432easd"
 type Parser a = ExceptT String (State String) a
 
+data JsonLikeValue =
+    JLString String |
+    JLInt Int |
+    JLMap [(String, JsonLikeValue)] |
+    JLArray [JsonLikeValue]
+    deriving (Show, Eq)
+
 -- parseAllMapedJLValues :: String -> String -> Either String (JsonLikeValue, String)
 -- parseAllMapedJLValues ('e':t) _ = Right (JLMap [], t)
 -- parseAllMapedJLValues str orgStr = 
@@ -44,17 +51,17 @@ type Parser a = ExceptT String (State String) a
 --                 (':':r) -> Right (take (read strLen) r, drop (read strLen) r)
 --                 _ -> Left ("Error around character " ++ show errPos ++ " received string: " ++ str ++ " Invalid string")
 
--- --parseJLValue :: String -> Either String (JsonLikeValue, String)
--- -- parseJLValue ('d':t) orgStr = 
---     -- case parseJLMap('d':t) orgStr of
---     --     Left a -> Left a
---     --     Right (a, b) -> Right (a, b)
--- -- parseJLValue ('l':t) orgStr = 
---     -- case parseJLArray [] ('l':t) orgStr of
---     --     Left a -> Left a
---     --     Right (a, b) -> Right (a, b)
+-- parseJLValue :: String -> Either String (JsonLikeValue, String)
+-- parseJLValue ('d':t) orgStr = 
+    -- case parseJLMap('d':t) orgStr of
+    --     Left a -> Left a
+    --     Right (a, b) -> Right (a, b)
+-- parseJLValue ('l':t) orgStr = 
+    -- case parseJLArray [] ('l':t) orgStr of
+    --     Left a -> Left a
+    --     Right (a, b) -> Right (a, b)
 -- parseJLValue ('i':t) = 
---     case parseInt of
+--     case parseJLInt of
 --         Left a -> Left a
 --         Right (a, b) -> Right (a, b)
 -- parseJLValue (h:t) =
@@ -88,12 +95,12 @@ type Parser a = ExceptT String (State String) a
 -- --     where
 -- --         errPos = lenDiff orgStr dnStartL
 
-parseString' = do
+parseJLString' = do
     str <- lift get
-    parseString str
+    parseJLString str
 
-parseString :: String -> Parser String
-parseString str = 
+parseJLString :: String -> Parser JsonLikeValue
+parseJLString str = 
     if isDigit $ head str then
         let
             strLen = takeWhile isDigit str
@@ -106,18 +113,18 @@ parseString str =
                 in 
                     do
                         lift $ put rest
-                        return parsedVal
+                        return $ JLString parsedVal
             _ -> throwE "Error String parser: symbol : wasn't found after a number"
     else throwE "Error String parser: first symbol isn't a number"
 
 
-parseInt' = 
+parseJLInt' = 
     do 
         str <- lift get
-        parseInt str
+        parseJLInt str
 
-parseInt :: String -> Parser Int
-parseInt str = 
+parseJLInt :: String -> Parser JsonLikeValue
+parseJLInt str = 
     case str of
         ('i':t) -> 
             if not (isDigit $ head t) then
@@ -134,7 +141,7 @@ parseInt str =
                             in
                                 do
                                     lift $ put r
-                                    return parsedVal
+                                    return $ JLInt parsedVal
                         _ -> throwE "Error Int parser: int didn't end with e"
         [] -> throwE "Error Int parser: Received an empty string"
         _  -> throwE "Error Int parser: Received string doesn't start with i"
