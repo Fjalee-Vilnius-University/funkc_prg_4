807cd93726527fb45b4ecebecb5de9cf7f7c18ff
Rytis <cepRytis@gmail.com>
2020-12-19 15:23:10 +0200

    function to find JLString in a JLMap

--- stack-project/src/Task4.hs
+++ stack-project/src/Task4.hs
@@ -189,20 +189,47 @@ parseJLInt' str =
 ------------------------------------------------------------------
 ------------------------------         ---------------------------
 ------------------------------------------------------------------
-
 test = 
     let
         --msg = "d4:prevd4:prevd4:lastld4:datali1ei0e1:Xeee4:prevd4:prevd4:lastld4:datali1ei2e1:Xeee4:prevd4:prevd4:lastld4:datali0ei1e1:Xeee4:prevd4:prevd4:lastld4:datali2ei2e1:Xeeee4:lastld4:datali2ei1e1:Oeeeee4:lastld4:datali2ei0e1:Oeeeee4:lastld4:datali1ei1e1:Oeeeee4:lastld4:datali0ei0e1:Oeeee4:lastld4:datali0ei2e1:Xeeee"
-        msg = "d4:last4:xoxo4:previ4ee"
-        myMap = fst $ p msg
-    in
-        fmap mapFindJLString myMap
         
+        --[(_, "oxi"),(_, 42),(_, "oxoxox")]
+        msg = "d3:qws3:oxi2:poi42e4:opep6:oxoxoxe"
+
+        --[(_, 32),(_, 42),(_, 67)]
+        --msg = "d3:qwsi32e2:poi42e4:opepi67ee"
 
 
-mapFindJLString :: JsonLikeValue -> Maybe JsonLikeValue 
-mapFindJLString (JLMap []) = Nothing
-mapFindJLString (JLMap (h:t)) = 
-    case snd h of
-        JLString a -> Just $ JLString a
-        _ -> mapFindJLString $ JLMap t
+        eitherMyMap = fst $ p msg
+    in
+        case eitherMyMap of
+            Left _ -> error "test Left"
+            Right myMap -> myFind mapFindJLString myMap
+        --fmap mapFindJLString' myMap
+
+type Finder a = ExceptT String (State JsonLikeValue) a
+
+myFind :: Finder a -> JsonLikeValue -> (Either String a, JsonLikeValue)
+myFind finder = runState (runExceptT finder)
+
+mapFindJLString :: Finder (JsonLikeValue, String)
+mapFindJLString = 
+    do 
+        myMap <- lift get
+        case myMap of
+            JLMap _ -> mapFindJLString' myMap
+            _ -> throwE "Error Finder: Not a map. "
+
+mapFindJLString' :: JsonLikeValue -> Finder (JsonLikeValue, String)
+mapFindJLString' (JLMap []) = do
+    lift $ put $ JLMap []
+    throwE "No String in the map. "
+mapFindJLString' (JLMap ((key1, val1):t)) = 
+    case val1 of
+        JLString _ -> do
+            lift $ put $ JLMap t
+            return $ (val1, "." ++ key1)
+        _ -> mapFindJLString' $ JLMap t
+
+
+
