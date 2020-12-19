304afb240f7a5958fe359a0bd5155e6a79584b46
Rytis <cepRytis@gmail.com>
2020-12-19 12:09:10 +0200

    parseJLArray fix

--- stack-project/src/Task4.hs
+++ stack-project/src/Task4.hs
@@ -62,13 +62,13 @@ parseJLArray :: Parser JsonLikeValue
 parseJLArray = do
     str <- lift get 
     case str of
-        ('l':t) -> parseJLArray' [] str
+        ('l':t) -> parseJLArray' [] t
         _ -> throwE "Not an Array. "
 
 parseJLArray' :: [JsonLikeValue] -> String -> Parser JsonLikeValue
 parseJLArray' acc str = 
     let
-        body = if (head str == 'l') then (drop 1 str) else str
+        body = str
         (eitherParsedVal, rest) = parse parseJLValue body
     in
         case eitherParsedVal of
@@ -142,51 +142,3 @@ parseJLInt' str =
                         _ -> throwE "Error Int parser: int didn't end with e. "
         [] -> throwE "Error Int parser: Received an empty string. "
         _  -> throwE "Error Int parser: Received string doesn't start with i. "
-
--- parseJLMap :: String -> String -> Either String (JsonLikeValue, String)
--- parseJLMap ('d':t) orgStr = 
---     case parseAllMapedJLValues t orgStr of
---         Left a -> Left a
---         Right (mapBody, rest) -> Right (mapBody, rest)
--- parseJLMap dnStartD orgStr = Left ("Error around character " ++ show errPos ++ " received string: " ++ dnStartD ++ " map has to start with a 'd'")
---     where 
---         errPos = lenDiff orgStr dnStartD
-
--- lenDiff :: String -> String -> Int
--- lenDiff str1 str2 = (length str1) - (length str2)
-
--- parseAllMapedJLValues :: String -> String -> Either String (JsonLikeValue, String)
--- parseAllMapedJLValues ('e':t) _ = Right (JLMap [], t)
--- parseAllMapedJLValues str orgStr = 
---     case parseMapedJLValue str orgStr of
---         Left a -> Left a
---         Right ((key, value), rest) -> 
---             case parseAllMapedJLValues rest orgStr of
---                 Left a -> Left a
---                 Right (JLMap acc, rest1) -> Right $ (JLMap ([(key, value)] ++ acc), rest1)
-
-
--- parseMapedJLValue :: String -> String -> Either String ((String, JsonLikeValue), String)
--- parseMapedJLValue str orgStr = 
---     case parseString str orgStr of
---         Left a -> Left a
---         Right (key, rest) ->
---             case parseJLValue rest orgStr of
---                 Left a -> Left a
---                 Right (value, rest') -> Right ((key, value), rest')
-
--- parseString :: String -> String -> Either String (String, String)
--- parseString str orgStr =
---     let
---         errPos = lenDiff orgStr str
---         strLen = if C.isDigit $ head str
---             then L.takeWhile C.isDigit str
---             else "not declared"
---         postfix = L.drop (length strLen) str
---     in
---         case strLen of 
---             "not declared" -> Left ("Error around character " ++ show errPos ++ " received string: " ++ str ++ " Length of the string was not declared")
---             _ ->
---                 case postfix of
---                 (':':r) -> Right (L.take (read strLen) r, L.drop (read strLen) r)
---                 _ -> Left ("Error around character " ++ show errPos ++ " received string: " ++ str ++ " Invalid string")
