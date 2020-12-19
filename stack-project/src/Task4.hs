02b6450a59747cb9380581ee00be4bd424413cbf
Rytis <cepRytis@gmail.com>
2020-12-19 13:23:22 +0200

    function for testing purposes

--- stack-project/src/Task4.hs
+++ stack-project/src/Task4.hs
@@ -1,3 +1,4 @@
+module Task4 where
 import Control.Monad.Trans.State.Strict
 import Control.Monad.Trans.Except
 import Control.Monad.Trans.Class
@@ -18,8 +19,46 @@ data JsonLikeValue =
     JLArray [JsonLikeValue]
     deriving (Show, Eq)
 
+
+p msg = parseJLMessage msg
+
+t = 
+    let
+        msg1 = "d2:xsl1:01:11:21:01:11:21:01:11:2e2:ysl1:01:01:01:11:11:11:21:21:2e2:vsl1:X1:X1:O1:X1:O1:X1:X1:O1:Oee"
+        msg2 = "d4:lastd2:vsl1:Xe2:ysli1ee2:xsli1eee4:prevd4:lastd2:vsl1:Oe2:ysli0ee2:xsli0eee4:prevd4:prevd4:lastd2:vsl1:Oe2:ysli0ee2:xsli1eee4:prevd4:lastd2:vsl1:Xe2:ysli0ee2:xsli1eeeee4:lastd2:vsl1:Xe2:ysli1ee2:xsli0eeeeee"
+        msg3 = "d4:prevd4:lastld4:datali0ei1e1:Oeee4:prevd4:lastld4:datali0ei2e1:Xeee4:prevd4:lastld4:datali2ei0e1:Oeee4:prevd4:prevd4:lastld4:datali2ei2e1:Oeee4:prevd4:prevd4:lastld4:datali0ei0e1:Oeee4:prevd4:lastld4:datali1ei1e1:Xeeeee4:lastld4:datali2ei1e1:Xeeeee4:lastld4:datali1ei2e1:Xeeeeeee4:lastld4:datali1ei0e1:Xeeee"
+    
+        test1 = p msg1
+        test2 = p msg2
+        test3 = p msg3
+        allTestRight = 
+            case test1 of
+                (Left _, _) -> False
+                (Right _, _) ->
+                    case test2 of 
+                    (Left _, _) -> False
+                    (Right _, _) ->
+                        case test3 of
+                            (Left _, _) -> False
+                            (Right _, _) -> True
+    in
+        do
+            putStrLn "======================================================================================================================================================================================================================================================================================"
+            putStrLn $ show test1
+            putStrLn ""
+            putStrLn "2"
+            putStrLn $ show test2
+            putStrLn ""
+            putStrLn "3"
+            putStrLn $ show test3
+            putStrLn ""
+            putStrLn $ show allTestRight
+
 parse :: Parser a -> String -> (Either String a, String)
 parse parser = runState (runExceptT parser)
+    
+parseJLMessage :: String -> (Either String JsonLikeValue, String)
+parseJLMessage msg = parse parseJLValue msg 
 
 parseJLValue :: Parser JsonLikeValue
 parseJLValue = parseJLString <|> parseJLInt <|> parseJLArray <|> parseJLMap
@@ -142,3 +181,4 @@ parseJLInt' str =
                         _ -> throwE "Error Int parser: int didn't end with e. "
         [] -> throwE "Error Int parser: Received an empty string. "
         _  -> throwE "Error Int parser: Received string doesn't start with i. "
+        
