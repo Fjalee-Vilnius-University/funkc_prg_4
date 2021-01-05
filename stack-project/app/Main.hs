8b9c126b2f3bf86f85a891352c9e6c858412b128
Rytis Cepulis <cepRytis@gmail.com>
2021-01-05 17:01:17 +0200

    deleted unneeded things

--- stack-project/app/Main.hs
+++ stack-project/app/Main.hs
@@ -7,4 +7,4 @@ main :: IO ()
 main = do
     msg <- getLine
     strArrToIO [""]
-    strArrToIO $ createStrForOutput $ simplifyMaybe $ msgToTops5 msg--"li0ei4eld0:5:AGIeG5:0CZc72:m41:40:4:DMCWi2e5:MPoPI3:3YLele2:EQ1:3el1:E3:oPHi2eee"
+    strArrToIO $ createStrForOutput $ simplifyMaybe $ msgToTops5 msg
